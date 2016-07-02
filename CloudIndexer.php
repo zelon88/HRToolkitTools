435ff725fc37e02482834561918acdad0b5ac0e0
zelon88 <zelon88@gmail.com>
2016-07-02 00:28:10 -0400

    Update CloudIndexer.php

--- CloudIndexer.php
+++ CloudIndexer.php
@@ -4,27 +4,30 @@
 <html>
 <body>
 <div align="center"><h1>Configuring your Cloud Drive ...</h1></div>
-<div align="center"><img src="http://cloud.honestrepair.net/wp-content/uploads/2016/03/Cloud-Banner.png" alt="HonestRepair Cloud Storage"</div>
-<div align="center"><img src="http://cloud.honestrepair.net/HRProprietary/pacman.gif" alt="PacMan is gathering information!" height="24" width="24"></div>
+<div align="center"><img src="http://localhost/wp-content/uploads/2016/03/Cloud-Banner.png" alt="HonestRepair Cloud Storage"</div>
+<div align="center"><img src="http://localhost/HRProprietary/pacman.gif" alt="PacMan is gathering information!" height="24" width="24"></div>
 
 <div align="center"><strong>Please wait while we authenticate your account and adjust the Cloud to your device...</strong></div>
 
 
-
+  <?php // / Set the number of pixels a screen needs to be to see page . ?>
 <script type="text/javascript">// < ![CDATA[
-if (screen.width <= 699) { document.location = "http://cloud.honestrepair.net/?page_id=52"; }
+	<?php // / Set the URL below to the page the user is sent to if their screensize is UNDER the specified number of pixels. ?>
+if (screen.width <= 699) { document.location = "http://localhost/PAGE_0"; }
 // ]]></script>
 
+  <?php // / Set the number of pixels a screen needs to be to see page 1. ?>
 <script type="text/javascript">// < ![CDATA[
 if (screen.width >= 700) {
-document.location = "http://cloud.honestrepair.net/?page_id=7";
+	<?php // / Set the URL below to the page the user is sent to if their screensize is OVER the specified number of pixels. ?>
+document.location = "http://localhost/PAGE_1";
 }
 // ]]></script>
 
 <script language=javascript>
 <!--
 if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-   location.replace("http://cloud.honestrepair.net/?page_id=52");
+   location.replace("http://localhost/PAGE_FOR_IPODS");
 }
 -->
 </script>
@@ -32,12 +35,12 @@ if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i
 <script language=javascript>
 <!--
 if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPad/i))) {
-   location.replace("http://cloud.honestrepair.net/?page_id=52");
+   location.replace("http://localhost/PAGE_FOR_IPADS");
 }
 -->
 </script>
 
-<meta http-equiv="refresh" content="5; url=http://cloud.honestrepair.net/>
+<meta http-equiv="refresh" content="5"; url="http://localhost/PAGE_IF_NOTHING_HAPPENS_FOR_SOME_REASON" >
 
 </body>
 </html>
